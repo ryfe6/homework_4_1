0113248870cd4d50c5daddb1a71dc0cd3bcd2523
ryfe <denis.koptelev@gmail.com>
2023-11-19 12:15:54 +0300

    add new func in class Item and add new test in test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -4,16 +4,10 @@ import pytest
 from src.item import Item
 
 
-# @pytest.mark.parametrize('name', "price", "quantity" "expected_result", [("Телевизор", 45000, 10, 450000),
-#                                                                          ("Ноутбук", 20000, 5, 100000)])
-# def test_class_Item(name, price, quantity, expected_result):
-#     item_1 = Item(name, price, quantity)
-#     # assert print(item_assert.calculate_total_price()) == expected_result
-#     i = item_1.price
-#     assert i == price
-
-@pytest.mark.parametrize("name, price, quantity, sell, expected_result", [("Телевизор", 45000, 10, 22500, 450000),
-                                                                          ("Ноутбук", 20000, 5, 10000, 100000)])
+@pytest.mark.parametrize(
+    "name, price, quantity, sell, expected_result",
+    [("Телевизор", 45000, 10, 22500, 450000), ("Ноутбук", 20000, 5, 10000, 100000)],
+)
 def test_class_Item(name, price, quantity, sell, expected_result):
     item_assert = Item(name, price, quantity)
     assert item_assert.calculate_total_price() == expected_result
@@ -33,3 +27,9 @@ def test_class_item_2():
     assert Item.string_to_number("6") == 6
     item.name = "Мегамощный телефон"
     assert item.name == "Мегамощный"
+
+
+def test_class_item_3():
+    item = Item("Монитор", 15000, 3)
+    assert repr(item) == "Item('Монитор', 15000, 3)"
+    assert str(item) == "Монитор"
