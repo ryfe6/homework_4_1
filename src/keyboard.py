bfa5324b060219e969aefda856424e95e4b95a47
ryfe <denis.koptelev@gmail.com>
2023-11-28 09:06:39 +0300

    fix class Mixin in keyboard

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,32 +3,30 @@ from src.item import Item
 
 class MixinLang:
     """Класс служит дополнением для класса Keyboard.
-       Позволяет изменять раскладку клавиатуры."""
+    Позволяет изменять раскладку клавиатуры."""
+
     def __init__(self) -> None:
         self._language = "EN"
 
-    def change_lang(self, _language: str) -> str:
+    def change_lang(self) -> str:
         """Метод изменяет раскладку клавиатуры"""
-        if _language == "EN":
+        if self._language == "EN":
             self._language = "RU"
             return self._language
         else:
             self._language = "EN"
         return self._language
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self._language
 
 
 class Keyboard(Item, MixinLang):
     """Класс для работы с товаром клавиатура."""
+
     def __init__(self, name: str, price: int, quantity: int, language: str = "EN") -> None:
         super().__init__(name, price, quantity)
         self._language = language
-
-    def change_lang(self) -> None:
-        """Метод для изменения раскладки клавиатуры."""
-        self._language = super().change_lang(self._language)
 
     @property
     def language(self) -> str:
